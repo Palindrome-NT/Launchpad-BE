843dd20b088e458c84bafe3d1822cbc2e0b6f2d6
naman <namantiwari04@gmail.com>
2025-11-06 15:27:28 +0530

    Revert last changes

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -37,7 +37,7 @@ export class UserController {
         res.cookie('accessToken', accessToken, {
           httpOnly: true,
           secure: process.env.NODE_ENV === 'production',
-          sameSite: 'strict',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
           path: '/',
           maxAge: 15 * 60 * 1000,
         });
@@ -45,7 +45,7 @@ export class UserController {
         res.cookie('refreshToken', refreshToken, {
           httpOnly: true,
           secure: process.env.NODE_ENV === 'production',
-          sameSite: 'strict',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
           path: '/',
           maxAge: 7 * 24 * 60 * 60 * 1000,
         });
